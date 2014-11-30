3de38f5c64205eea778581b0429ca9b7dad41ccd
Danielle Kain <daniellekain@dn51uc8e.sunet>
2014-11-30 12:41:57 -0800

    no more of this type == 1 nonsense -- also edited out the extensiony things for now so we can focus on basic functionality

--- src/quiz/QuizConstants.java
+++ src/quiz/QuizConstants.java
@@ -4,4 +4,11 @@ public class QuizConstants {
 	public static final String QUIZ_SITE_TITLE = "QuizFeed"; // TODO come up with a better name
     public static final String INDEX_FILE = "index.jsp";
     public static final String HEADER_FILE = "header.jsp";
+    public static final int TEXT_RESPONSE = 1;
+    public static final int FILL_IN_BLANK = 2;
+    public static final int MULT_CHOICE = 3;
+    public static final int PICTURE_RESPONSE = 4;
+    public static final int MULTI_TEXT_RESPONSE = 5;
+    public static final int MULTI_CHOICE_ANSWER = 6;
+    public static final int MATCHING = 7;
 }
